7ebd33b65a7e357df4b419756111566941c09c81
andreadgobbo@gmail.com <andreadgobbo@gmail.com>
2020-10-14 08:48:08 +0200

    Esercizio che esegue la trasformazione binaria

---
+++ trasformazione binario/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace trasformazione_binario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, resto;
+            Console.WriteLine("inserire il dato");
+            num = int.Parse(Console.ReadLine());
+            string binario = "";
+            while (num > 0)
+            {
+                resto = num % 2;
+                num /= 2;
+                binario = Convert.ToString(resto) + binario;
+
+            }
+            Console.WriteLine($"il numero in binario è {binario}");
+            Console.ReadLine();
+        }
+    }
+}
