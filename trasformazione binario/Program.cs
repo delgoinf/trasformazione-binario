59a71bfc85c94c57669693ea63714057d6cab404
andreadgobbo@gmail.com <andreadgobbo@gmail.com>
2020-10-15 10:24:45 +0200

    convertitore base decimale

--- trasformazione binario/Program.cs
+++ trasformazione binario/Program.cs
@@ -6,19 +6,52 @@ namespace trasformazione_binario
     {
         static void Main(string[] args)
         {
-            int num, resto;
-            Console.WriteLine("inserire il dato");
-            num = int.Parse(Console.ReadLine());
-            string binario = "";
+            int num, resto, valorebase;
+            string numeroconvertito = "";
+            Console.WriteLine("inserisci la base da convertire");
+            valorebase = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("inserire il dato da convertire");
+            num = Convert.ToInt32(Console.ReadLine());
             while (num > 0)
             {
-                resto = num % 2;
-                num /= 2;
-                binario = Convert.ToString(resto) + binario;
+                resto = num % valorebase;
+                if (resto == 15)
+                {
+                    numeroconvertito += "f";
+                }
+                else if (resto == 14)
+                {
+                    numeroconvertito += "e";
+                }
+                else if (resto == 13)
+                {
+                    numeroconvertito += "d";
+                }
+                else if (resto == 13)
+                {
 
+                }
+                else if (resto == 12)
+                {
+                    numeroconvertito += "c";
+                }
+                else if (resto == 11)
+                {
+                    numeroconvertito += "b";
+                }
+                else if (resto == 10)
+                {
+                    numeroconvertito += "a";
+                }
+                else
+                {
+                    numeroconvertito = Convert.ToString(resto) + numeroconvertito;
+                }
+                num = num / valorebase;
             }
-            Console.WriteLine($"il numero in binario è {binario}");
+            Console.WriteLine($"il numero convertito in base {valorebase} è : {numeroconvertito}");
             Console.ReadLine();
+
         }
     }
 }
